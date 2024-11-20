c4ac2c00ede6d2743da481153f3c605a6ff591d5
DESKTOP-D02M8EQ\RexWei <a5839813@gmail.com>
2024-11-20 20:13:46 +0800

    +) 環境部屬(推上Github page)

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import AppRoutes from './routes';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // 引入 Bootstrap JavaScript 依賴
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// 使用 process.env.PUBLIC_URL 作為 basename
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <AppRoutes />
     </BrowserRouter>
   </React.StrictMode>
